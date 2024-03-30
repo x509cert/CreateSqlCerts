7b292a00ddb212df5710b3a1bff09e0e2c69e99c
Michael Howard <mikehow@microsoft.com>
2024-03-30 14:56:46 -0500

    More edits to Next Steps

--- CreateSqlCerts/Program.cs
+++ CreateSqlCerts/Program.cs
@@ -125,14 +125,14 @@ Console.WriteLine($"Server cert and private key is in {serverCertFilename} encry
 #endregion
 
 #region Next Steps
-Console.WriteLine("Next steps.");
+Console.WriteLine("\n\n**NEXT STEPS**");
 Console.WriteLine("1. SET KEY ACL");
 Console.WriteLine(" You need to set the ACL on the server's private key so SQL Server can read the key.");
 Console.WriteLine(" Open the certlm.msc, the Local Machine cert store tool.");
 Console.WriteLine($" Expand Personal, Certificates and right click the cert in question, issued to '{hostName}'");
 Console.WriteLine(" Select All Tasks and then click Manage Private Keys.");
 Console.WriteLine(" Click Add to add the SQL Server service account (probably 'NT Service\\MSSQLServer' and give it Read permission.");
-Console.WriteLine("You can check the service account name is correct by clicking Check Names.");
+Console.WriteLine(" You can check the service account name is correct by clicking Check Names.");
 
 Console.WriteLine("\n2. CONFIGURE SQL SERVER");
 Console.WriteLine(" You need to configure SQL Server to use the certificate and private key.");
